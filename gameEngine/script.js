2db3b6fcd19b769acda367c3616d463121bb2c62
Bennett Parker <143557544+broicool@users.noreply.github.com>
2025-12-16 14:01:37 -0700

    new update

--- gameEngine/script.js
+++ gameEngine/script.js
@@ -1,7 +1,13 @@
 var c = document.getElementById("gameCanvas");
 var addToCanvas = c.getContext("2d");
+const keys = {
+    w : true,
+};
+window.addEventListener('keydown', (e) => { keys[e.key] = true; });
+window.addEventListener('keyup', (e) => { keys[e.key] = false; });
+
 const gameEngine = {
-    setUpCanvas : function(w,h) {
+    setUpCanvas : function(w,h){
         c.width = window.innerWidth;
         c.height = window.innerHeight;
         c.style.width = w;
@@ -16,8 +22,28 @@ const gameEngine = {
         rectangle : function(x,y,l,h){
             addToCanvas.fillRect(x,y,l,h);
         }
+    },
+    fill : {
+        hexcode : function(color){
+            addToCanvas.fillStyle = color;
+        },
+        RGB : function(r,g,b){
+            addToCanvas.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        },
+        name : function(color){
+            addToCanvas.fillStyle = color;
+        },
+        clear : function(){
+            addToCanvas.fillStyle = "black";
+        }
+    },
+    detectKey : function(key){
+        return keys[key];
     }
 }
+
 gameEngine.setUpCanvas("100%", "100%");
-gameEngine.draw.circle(0,0,50,50)
-gameEngine.draw.rectangle(300,300,50,100)
+gameEngine.fill.name("red");
+gameEngine.draw.circle(0,0,50,50);
+gameEngine.fill.clear();
+gameEngine.draw.rectangle(300,300,50,100);
